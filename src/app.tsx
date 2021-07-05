ae26baf1b45e39e57fc891551270bb7bc8748834
Yuhao Chen <yuhaochen0107@gmail.com>
2021-07-05 15:14:27 -0400

    feat: inital commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './app.less';
+
+const App: React.FC<Record<string, never>> = () => (
+  <div className="app">Welcome to Alpha Cc Web</div>
+);
+
+export default App;
