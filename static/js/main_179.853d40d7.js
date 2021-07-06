fcaa33e4c22108157ee90cfad4e6ff90f5b4c21e
Yuhao-C <Yuhao-C@users.noreply.github.com>
2021-07-06 02:18:04 +0000

    deploy: 22c602cc021ccc4f54b26df59d28bd9e65f75f8a

---
+++ static/js/main_179.853d40d7.js
@@ -0,0 +1,40 @@
+(self["webpackChunkalpha_cc_web"] = self["webpackChunkalpha_cc_web"] || []).push([[179],{
+
+/***/ 177:
+/***/ ((__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(294);
+// EXTERNAL MODULE: ./node_modules/react-dom/index.js
+var react_dom = __webpack_require__(935);
+;// CONCATENATED MODULE: ./src/app.tsx
+
+
+
+var App = function App() {
+  return /*#__PURE__*/react.createElement("div", {
+    className: "app"
+  }, "Welcome to Alpha Cc Web");
+};
+
+/* harmony default export */ const app = (App);
+;// CONCATENATED MODULE: ./src/index.tsx
+
+
+
+react_dom.render( /*#__PURE__*/react.createElement(react.StrictMode, null, /*#__PURE__*/react.createElement(app, null)), document.getElementById('root'));
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [935], () => (__webpack_exec__(177)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=main_179.853d40d7.js.map
